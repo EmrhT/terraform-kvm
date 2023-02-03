9ec13ca4520c3a63b4450cd6a7fd4a7e0bb6f377
emrht <emrhtfn@gmail.com>
2023-02-03 23:37:31 +0300

    first domain added

--- main.tf
+++ main.tf
@@ -15,6 +15,46 @@ terraform {
 }
 
 provider "libvirt" {
-  uri = "qemu+ssh://root@kvmhost.example.com/system"    
-  # Configuration options
+  uri = "qemu+ssh://root@kvmhost.example.com:53330/system"    
+}
+
+resource "libvirt_volume" "centos7-qcow2" {
+  name = "centos7.qcow2"
+  pool = "default" # List storage pools using virsh pool-list
+  #source = "https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2"
+  source = "/opt/CentOS-7-x86_64-GenericCloud.qcow2"
+  format = "qcow2"
+}
+
+# Define KVM domain to create
+resource "libvirt_domain" "centos7" {
+  name   = "centos7"
+  memory = "2048"
+  vcpu   = 2
+  qemu_agent  = "true"
+  
+  network_interface {
+    network_name = "default" # List networks with virsh net-list
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.centos7-qcow2.id}"
+  }
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+}
+
+# Output Server IP
+output "ip" {
+  value = "${libvirt_domain.centos7.network_interface.0.addresses.0}"
 }
